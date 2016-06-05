366274fc920cdaf5e083d3a529f56c5c96544c46
Abrar Basha <abrar@Abrars-MacBook.local>
2016-06-05 01:36:21 -0400

    updated python calculator. accurate calculation only yields 11 decimal places

--- picalculator.py
+++ picalculator.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import sys, decimal
+import sys, decimal, math
 from decimal import *
 
 # Uses the Bailey-Borwein-Plouffe formula
@@ -8,22 +8,27 @@ def calculatePi(i):
     D = decimal.Decimal
     with decimal.localcontext() as ctx:
         ctx.prec = i + 1
-        # ctx.rounding = ROUND_FLOOR # weird issue where this is rounding up when it shouldn't.
         pi = sum(1/Decimal(16)**k *
 		(Decimal(4)/(8*k+1) - Decimal(2)/(8*k+4) - Decimal(1)/(8*k+5) - Decimal(1)/(8*k+6))
 		# k is the amount of iterations of the summation. I selected 100
-		for k in range (100))
+		for k in range (100)) 
     return pi
 
+def truncate(f, n):
+    return math.floor(f * 10 ** n) / 10 ** n
 
 def shell():
 	entry = int(input("Welcome to Abrar Basha's Pi Calculator. Enter the number of digits of Pi you want to see or enter 0 to exit\n"))
-
+    
 	while True:
 		if entry == 0:
 			break
+		elif isinstance(entry, str):
+			entry = int(input("This is not a number. Enter the number of digits of Pi you want to see or enter 0 to exit\n"))
 		else:
-			print (calculatePi(entry))
+			finalValue = truncate(calculatePi(entry+1), entry)
+			print (finalValue)
+			#print(caculatePi(entry)) #use this instead of the previous two lines if you want more than 11 digits. not really sure why the truncate is confining me to 11 digits. 
 			entry = int(input("Enter the number of digits of Pi you want to see or enter 0 to exit\n"))
 
 if __name__=='__main__':
